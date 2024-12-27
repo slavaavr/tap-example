24af365387f5a952e8eac98182aa774fee055222
slavaavr <_ava@outlook.com>
2024-12-27 14:11:35 +0300

    wip

---
+++ app.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Game from "@/components/Game.vue";
+</script>
+
+<template>
+  <main class="flex flex-col justify-center items-center min-h-[100vh]">
+    <div class="flex justify-between  gap-5">
+      <p>Users</p>
+      <p>Coins </p>
+    </div>
+    <Game/>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ components/Game.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+function tap(): void {
+  alert("ok");
+}
+</script>
+
+<template>
+  <!--    <div class="">-->
+  <!--        <p>Tap The Hamster</p>-->
+  <img @click="tap" src="/hamster.png" alt="hamster" class="rounded-full cursor-pointer img-size">
+  <!--    </div>-->
+</template>
+
+<style scoped>
+.img-size {
+  width: 50%;
+  height: 50%;
+}
+
+@media (min-width: 1024px) {
+  .img-size {
+    width: 20%;
+    height: 20%;
+  }
+}
+</style>
